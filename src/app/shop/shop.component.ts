c60636b66aaecfed7602c4eef824e3aff398d72e
Ravuthz <ravuthz@gmail.com>
2016-12-23 22:44:54 +0700

    Basic List movie, added to cart

--- src/app/shop/shop.component.ts
+++ src/app/shop/shop.component.ts
@@ -1,15 +1,33 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-shop',
-  templateUrl: './shop.component.html',
-  styleUrls: ['./shop.component.css']
+    selector: 'app-shop',
+    templateUrl: './shop.component.html',
+    styleUrls: ['./shop.component.css']
 })
+
 export class ShopComponent implements OnInit {
+    public recentMovie: String;
+    public cartItem: Array<String>;
+    public movieList: Array<String>;
 
-  constructor() { }
+    constructor() {
+        this.recentMovie = "None";
+        this.cartItem = [];
+        this.movieList = [
+            'Batman Vs Superman',
+            'Civil War',
+            'Dead Pool',
+            'Spiderman'
+        ];
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {}
+
+    selectedMovie(movie) {
+        console.log(movie);
+        this.recentMovie = movie;
+        this.cartItem.push(movie);
+    }
 
 }
